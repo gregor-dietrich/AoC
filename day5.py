b732635d4a86f7ad785dbdf9e875477f864cf2ab
Gregor Dietrich <11874515+gregor-dietrich@users.noreply.github.com>
2023-12-06 21:34:42 +0100

    Day 5 minor edit

--- day5.py
+++ day5.py
@@ -1,17 +1,11 @@
-from datetime import datetime
-
 data = {}
 
 
 def seed_to_location(seed: int) -> int:
-    soil = lookup_data(seed, "seed-to-soil")
-    fertilizer = lookup_data(soil, "soil-to-fertilizer")
-    water = lookup_data(fertilizer, "fertilizer-to-water")
-    light = lookup_data(water, "water-to-light")
-    temperature = lookup_data(light, "light-to-temperature")
-    humidity = lookup_data(temperature, "temperature-to-humidity")
-    location = lookup_data(humidity, "humidity-to-location")
-    return location
+    return lookup_data(lookup_data(lookup_data(lookup_data(lookup_data(lookup_data(lookup_data(
+        seed, "seed-to-soil"), "soil-to-fertilizer"), "fertilizer-to-water"),
+        "water-to-light"), "light-to-temperature"), "temperature-to-humidity"),
+        "humidity-to-location")
 
 
 def lookup_data(key: int, data_type: str = "") -> int:
@@ -19,9 +13,7 @@ def lookup_data(key: int, data_type: str = "") -> int:
         if key < source_start or key > source_start + data[data_type][source_start][1] - 1:
             continue
         destination_start = data[data_type][source_start][0]
-        # print(f"Lookup: Key={key}, Data={data_type}, Result={destination_start + key - source_start}")
         return destination_start + key - source_start
-    # print(f"Lookup: Key={key}, Data={data_type}, Result={key}")
     return key
 
 
@@ -50,7 +42,7 @@ for seed in data["seeds"]:
     locations.append(seed_to_location(seed))
 print(min(locations))
 
-# Part 2
+# Part 2 (functional, but horribly inefficient)
 nearest_location = float('inf')
 for i in range(1, len(data["seeds"]), 2):
     start, end = int(data["seeds"][i - 1]), int(data["seeds"][i - 1]) + int(data["seeds"][i])
