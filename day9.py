7c072b9c9f907be411a181310bc2ef9590cfc284
Gregor Dietrich <11874515+gregor-dietrich@users.noreply.github.com>
2023-12-09 22:40:58 +0100

    Day 9 refactoring

--- day9.py
+++ day9.py
@@ -1,19 +1,23 @@
+def process_sequences(sequence: str) -> (int, int):
+	sequence = [[int(n) for n in sequence.strip().split()]]
+	while not all(item == 0 for item in sequence[-1]):
+		next_sequence = []
+		for i in range(1, len(sequence[-1])):
+			next_sequence.append(sequence[-1][i] - sequence[-1][i - 1])
+		sequence.append(next_sequence)
+	next_val, previous_val = 0, sequence[0][0]
+	for i in range(1, len(sequence))[::-1]:
+		next_val += sequence[i - 1][-1]
+		previous_val = sequence[i - 1][0] - sequence[i][0]
+		sequence[i - 1].insert(0, sequence[i - 1][0] - sequence[i][0])
+	return next_val, previous_val
+
+
 sum_of_next_values, sum_of_previous_values = 0, 0
 with open("inputs/day9.txt") as file:
 	for line in file:
-		sequences = [[int(n) for n in line.strip().split()]]
-		while not all(item == 0 for item in sequences[-1]):
-			next_sequence = []
-			for i in range(1, len(sequences[-1])):
-				next_sequence.append(sequences[-1][i] - sequences[-1][i - 1])
-			sequences.append(next_sequence)
-		next_value, previous_value = 0, sequences[0][0]
-		for i in range(1, len(sequences))[::-1]:
-			next_value += sequences[i - 1][-1]
-			previous_value = sequences[i - 1][0] - sequences[i][0]
-			sequences[i - 1].insert(0, sequences[i - 1][0] - sequences[i][0])
+		next_value, previous_value = process_sequences(line)
 		sum_of_next_values += next_value
 		sum_of_previous_values += previous_value
-		sequences[0].append(next_value)
 print(sum_of_next_values)
 print(sum_of_previous_values)
