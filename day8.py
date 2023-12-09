879983c8645728ac53113e31ee11a9c9721ec544
Gregor Dietrich <11874515+gregor-dietrich@users.noreply.github.com>
2023-12-09 21:09:03 +0100

    Day 8 Part 2

--- day8.py
+++ day8.py
@@ -1,20 +1,38 @@
-instructions, node_map, first = None, {}, True
+instructions, node_map, current_node, current_nodes, steps = None, {}, "AAA", [], 0
 with open("inputs/day8.txt") as file:
 	for line in file:
 		if line.strip() == "":
 			continue
-		if first:
+		if instructions is None:
 			instructions = line.strip()
-			first = False
 			continue
 		node_map[line[:3]] = [line[7:10], line[12:15]]
-steps, current_node = 0, "AAA"
+		if line[2] == "A":
+			current_nodes.append(line[:3])
+
+# Part 1
 while current_node != "ZZZ":
 	for i in range(len(instructions)):
 		if current_node == "ZZZ":
 			break
 		steps += 1
 		current_node = node_map[current_node][1] if instructions[i] == "R" else node_map[current_node][0]
-		if i == len(instructions) - 1:
-			i = 0
+		i = 0 if i == len(instructions) - 1 else i
+print(steps)
+
+
+# Part 2
+def goal_reached() -> bool:
+	for node in current_nodes:
+		if node[2] != "Z":
+			return False
+	return True
+
+
+steps, i = 0, 0
+while not goal_reached():
+	steps += 1
+	for j in range(len(current_nodes)):
+		current_nodes[j] = node_map[current_nodes[j]][1] if instructions[i] == "R" else node_map[current_nodes[j]][0]
+	i = 0 if i == len(instructions) - 1 else i + 1
 print(steps)
