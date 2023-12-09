499fab521b9773f9d9077ad06b7188bf8ddba9ff
Gregor Dietrich <11874515+gregor-dietrich@users.noreply.github.com>
2023-12-09 22:22:30 +0100

    Day 9 Part 2

--- day9.py
+++ day9.py
@@ -1,14 +1,11 @@
-from typing import List
-
-
-def sequence_finished(sequence: List['int']) -> bool:
-	for item in sequence:
+def sequence_finished(seq) -> bool:
+	for item in seq:
 		if item != 0:
 			return False
 	return True
 
 
-sum_of_next_values = 0
+sum_of_next_values, sum_of_previous_values = 0, 0
 with open("inputs/day9.txt") as file:
 	for line in file:
 		sequences = [[int(n) for n in line.strip().split()]]
@@ -17,8 +14,14 @@ with open("inputs/day9.txt") as file:
 			for i in range(1, len(sequences[-1])):
 				next_sequence.append(sequences[-1][i] - sequences[-1][i - 1])
 			sequences.append(next_sequence)
-		next_value = 0
+		next_value, previous_value = 0, sequences[0][0]
 		for sequence in sequences:
 			next_value += sequence[-1]
+		for i in range(1, len(sequences) - 1)[::-1]:
+			previous_value = sequences[i - 1][0] - sequences[i][0]
+			sequences[i - 1].insert(0, sequences[i - 1][0] - sequences[i][0])
 		sum_of_next_values += next_value
+		sum_of_previous_values += previous_value
+		sequences[0].append(next_value)
 print(sum_of_next_values)
+print(sum_of_previous_values)
